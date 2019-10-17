d66f3c6c111eb49b6d82a6ff710f785d106a3637
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-17 11:39:48 +0300

    Update invite.js

--- commands/moderation/invite.js
+++ commands/moderation/invite.js
@@ -3,13 +3,13 @@ const Discord = require("discord.js");
 module.exports = {
     name: "invite",
     category: "moderation",
-    aliases: ["c"],
+    aliases: ["inv"],
     description: "sends bot invite link.",
     run: (client, message, args) => {
         message.delete();
         
         if(message.member.hasPermission("MANAGE_MESSAGES")) {
         message.member.send("https://discordapp.com/api/oauth2/authorize?client_id=603517038375206923&permissions=8&scope=bot");
-    }else message.channel.send("You don't have required permissions to access this command!");
+    }else message.channel.send("Use this to invite me:\nYou don't have required permissions to access this command!");
 }
 }
