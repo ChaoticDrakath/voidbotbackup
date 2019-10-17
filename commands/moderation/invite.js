9c3b0f4cea4730ffae278d5018d553749489f3f0
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-17 11:37:46 +0300

    Update invite.js

--- commands/moderation/invite.js
+++ commands/moderation/invite.js
@@ -1,16 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "clear",
+    name: "invite",
     category: "moderation",
     aliases: ["c"],
-    description: "clears chat (max messages 99)",
-    usage: "[amount]",
+    description: "sends bot invite link.",
     run: (client, message, args) => {
         message.delete();
         
         if(message.member.hasPermission("MANAGE_MESSAGES")) {
-        message.channel.send("https://discordapp.com/api/oauth2/authorize?client_id=603517038375206923&permissions=8&scope=bot");
+        message.member.send("https://discordapp.com/api/oauth2/authorize?client_id=603517038375206923&permissions=8&scope=bot");
     }else message.channel.send("You don't have required permissions to access this command!");
 }
 }
