421b3b39cba0cffd5db8caea5793a761c678b2cc
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-17 10:13:45 +0300

    Update leave.js

--- commands/moderation/leave.js
+++ commands/moderation/leave.js
@@ -12,7 +12,5 @@ module.exports = {
     if(!message.guild.id.size < 1) return message.reply("You must supply a Guild ID");
     if (message.author.id !== ops.ownerID) return;
     message.guild.leave();
-        .then(g => console.log(`Left the guild ${g}`));
-         .catch(console.error);
 }
 }
