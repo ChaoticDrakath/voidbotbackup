c499f4dd6eeba10467e112c56a8401e16e3d8935
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-10-31 14:32:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ client.on("guildCreate", guild => {
 
 client.on("message", async message => {
     const prefix = "V"
+    
 
     if (message.author.bot) return;
-    if (!message.guild) return;
     if (!message.content.toLowerCase().startsWith(prefix.toLowerCase())) return;
-    if (!message.member) message.member = await message.guild.fetchMember(message);
+    
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
@@ -50,10 +50,16 @@ client.on("message", async message => {
     if (cmd.length === 0) return;
     
     let command = client.commands.get(cmd);
+    
     if (!command) command = client.commands.get(client.aliases.get(cmd));
 
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }    
+    
+
     if (command) 
-        command.run(client, message, args);
+        command.run(client, message, args);   
 });
 
 client.login(process.env.TOKEN);
